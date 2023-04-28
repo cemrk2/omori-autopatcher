9ce8e8545e4f5c102feafb31b9ff413851cc46f6
cemrk2 <cemrk2@protonmail.com>
2023-04-28 18:47:16 +0200

    Added support for HiDPI

--- omori-autopatcher/Form1.Designer.cs
+++ omori-autopatcher/Form1.Designer.cs
@@ -39,10 +39,10 @@
             // 
             // statusLbl
             // 
-            this.statusLbl.Location = new System.Drawing.Point(0, 74);
-            this.statusLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.statusLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusLbl.Location = new System.Drawing.Point(0, 91);
             this.statusLbl.Name = "statusLbl";
-            this.statusLbl.Size = new System.Drawing.Size(551, 29);
+            this.statusLbl.Size = new System.Drawing.Size(543, 36);
             this.statusLbl.TabIndex = 4;
             // 
             // dumpExeBtn
@@ -53,8 +53,8 @@
             this.dumpExeBtn.Depth = 0;
             this.dumpExeBtn.HighEmphasis = true;
             this.dumpExeBtn.Icon = null;
-            this.dumpExeBtn.Location = new System.Drawing.Point(5, 108);
-            this.dumpExeBtn.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.dumpExeBtn.Location = new System.Drawing.Point(7, 133);
+            this.dumpExeBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.dumpExeBtn.MouseState = MaterialSkin.MouseState.HOVER;
             this.dumpExeBtn.Name = "dumpExeBtn";
             this.dumpExeBtn.NoAccentTextColor = System.Drawing.Color.Empty;
@@ -74,8 +74,8 @@
             this.dumpBtn.Depth = 0;
             this.dumpBtn.HighEmphasis = true;
             this.dumpBtn.Icon = null;
-            this.dumpBtn.Location = new System.Drawing.Point(170, 108);
-            this.dumpBtn.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.dumpBtn.Location = new System.Drawing.Point(171, 133);
+            this.dumpBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.dumpBtn.MouseState = MaterialSkin.MouseState.HOVER;
             this.dumpBtn.Name = "dumpBtn";
             this.dumpBtn.NoAccentTextColor = System.Drawing.Color.Empty;
@@ -95,8 +95,8 @@
             this.patchBtn.Depth = 0;
             this.patchBtn.HighEmphasis = true;
             this.patchBtn.Icon = null;
-            this.patchBtn.Location = new System.Drawing.Point(287, 108);
-            this.patchBtn.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.patchBtn.Location = new System.Drawing.Point(287, 132);
+            this.patchBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.patchBtn.MouseState = MaterialSkin.MouseState.HOVER;
             this.patchBtn.Name = "patchBtn";
             this.patchBtn.NoAccentTextColor = System.Drawing.Color.Empty;
@@ -110,14 +110,15 @@
             // 
             // unpatchBtn
             // 
+            this.unpatchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.unpatchBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.unpatchBtn.BackColor = System.Drawing.SystemColors.Control;
             this.unpatchBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
             this.unpatchBtn.Depth = 0;
             this.unpatchBtn.HighEmphasis = true;
             this.unpatchBtn.Icon = null;
-            this.unpatchBtn.Location = new System.Drawing.Point(410, 108);
-            this.unpatchBtn.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.unpatchBtn.Location = new System.Drawing.Point(409, 133);
+            this.unpatchBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.unpatchBtn.MouseState = MaterialSkin.MouseState.HOVER;
             this.unpatchBtn.Name = "unpatchBtn";
             this.unpatchBtn.NoAccentTextColor = System.Drawing.Color.Empty;
@@ -131,22 +132,21 @@
             // 
             // progressBar
             // 
-            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar.Depth = 0;
-            this.progressBar.Location = new System.Drawing.Point(-2, 67);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar.Location = new System.Drawing.Point(-3, 77);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.progressBar.MouseState = MaterialSkin.MouseState.HOVER;
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(553, 5);
+            this.progressBar.Size = new System.Drawing.Size(554, 5);
             this.progressBar.TabIndex = 12;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(549, 154);
+            this.ClientSize = new System.Drawing.Size(550, 176);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.unpatchBtn);
             this.Controls.Add(this.patchBtn);
@@ -154,15 +154,14 @@
             this.Controls.Add(this.dumpExeBtn);
             this.Controls.Add(this.statusLbl);
             this.Location = new System.Drawing.Point(15, 15);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(2, 52, 2, 2);
+            this.Padding = new System.Windows.Forms.Padding(3, 64, 3, 2);
             this.Text = "OMORI AutoPatcher";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         private MaterialSkin.Controls.MaterialProgressBar progressBar;
